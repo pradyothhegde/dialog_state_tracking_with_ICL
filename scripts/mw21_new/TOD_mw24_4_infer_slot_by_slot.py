927d0e7803b8164746cd6a40d428d057fc7168f0
Hegde Pradyoth <hegde@merlin.fit.vutbr.cz>
2024-12-20 16:24:18 +0100

    Add scripts folder

---
+++ scripts/mw21_new/TOD_mw24_4_infer_slot_by_slot.py
@@ -0,0 +1,358 @@
+# sample run: 
+# export CUDA_VISIBLE_DEVICES=$(free-gpus.sh 1); export HF_HUB_OFFLINE=1; export HF_DATASETS_OFFLINE=1; export TRANSFORMERS_OFFLINE=1; export HF_EVALUATE_OFFLINE=1; python TOD_mw24_4_infer_slot_by_slot1.py
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteria, StoppingCriteriaList
+import numpy as np
+import argparse
+import ast
+import json
+import os
+import json_repair
+import platform
+
+from TODx import finding_test_labels, remove_punctuations
+a = torch.rand(1, 1).cuda()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='allenai/OLMo-7B-Instruct', help='model name')  # allenai/OLMo-7B-Instruct | mistralai/Mistral-7B-Instruct-v0.3
+    parser.add_argument('--nec_files', type=str, default='/mnt/matylda4/hegde/int_ent/TOD_llm/experiments/NecessaryFiles/')
+    parser.add_argument('--output_folder_path', type=str, default='/mnt/matylda4/hegde/int_ent/TOD_llm/experiments/mw24/O7BI/')
+    parser.add_argument('--out_folder_prefix', type=str, default='TOD_')
+    parser.add_argument('--context_number', type=int, default=10)
+
+    parser.add_argument('--input_file', type=str,       default='LLM_input_U_PY_TY_agent_added_n_limited_nn.txt', help='Check necessary files folder for the appropriate file')
+    parser.add_argument('--dialog_side', type=str,      default='UA', help='U=user, UA=user-agent')
+    parser.add_argument('--punct', type=str,            default='N', help='Y=yes, N=no')
+    parser.add_argument('--tag', type=str,              default='Y', help='Y=yes, N=no')
+    parser.add_argument('--instruction', type=int,      default=2, help='0=no instruction, 1=dynamic instruction, 2 = static instruction')
+
+    parser.add_argument('--offset_start', type=int, default=0, help='Start offset for the test file')
+    parser.add_argument('--offset_add', type=int, default=8000, help='Add offset for the test file')
+    return parser.parse_args()
+
+# output config file
+config_string = '''
+This is the configuration file for the run.
+TOD_mw24_O7BI_SD_I1_10_PY_TY filename indicates the following:
+TOD - Task of Dialogue
+mw24 - MultiWOZ 2.4 dataset
+O7BI - OLMo 7B Instruct model
+SS - Slot Detection based on domain (domain wise, open ended), SS - Slot Detection based on slot by slot
+I1 - 0 = No instruction, Instruction type 1 (Dynamic instruction), 2 = Static instruction
+10 - Maximum context number
+PY - Punctuation Yes
+TY - Tagging Yes
+This is extracted from the input file.
+'''
+
+
+
+
+# dom2slots = {
+#     'taxi': ['leaveAt', 'destination', 'departure', 'arriveBy'], 
+#     'restaurant': ['people', 'day', 'time', 'food', 'pricerange', 'name', 'area'], 
+#     'attraction': ['type', 'name', 'area'], 
+#     'train': ['people', 'leaveAt', 'destination', 'day', 'arriveBy', 'departure'], 
+#     'hotel': ['stay', 'day', 'people', 'name', 'area', 'parking', 'pricerange', 'stars', 'internet', 'type']
+# }
+
+dom2slots = {
+    'taxi': ['arriveBy', 'departure', 'destination', 'leaveAt'], 
+    'restaurant': ['area', 'day', 'food', 'name', 'people', 'pricerange', 'time'], 
+    'attraction': ['area', 'name', 'type'], 
+    'train': ['arriveBy', 'day', 'departure', 'destination', 'leaveAt', 'people'], 
+    'hotel': ['area', 'day', 'internet', 'name', 'parking', 'people', 'pricerange', 'stars', 'stay', 'type']
+}
+
+
+def get_instruction_for_domain(domain):
+    domain_processing_string = "[" + domain + "]"
+    # domain_list = ast.literal_eval(domain_processing_string)
+    # print(type(domain))
+    # print(domains) # domains is a list
+    # build instruction
+    # Instruction: Identify slot names and values from the dialogue. The domain is taxi. The slots for taxi are leaveAt, destination, departure, arriveBy.
+    instruction = "Identify slot names and values from the dialogue. The domain is " + domain + "." + " The slots for " + domain + " are "
+    for j in range(len(dom2slots[domain])):
+        instruction += dom2slots[domain][j]
+        if j != len(dom2slots[domain]) - 1:
+            instruction += ", "
+        else:
+            instruction += "."
+    return instruction
+
+    # pass
+
+def get_instruction(process_line):
+    # find domain
+    # print("Process line: ", process_line)
+    process_line = ast.literal_eval(process_line)[0]
+    # Domain: ["taxi"] Slots: {
+    temp_text = process_line.split('\n')
+    # get the last element in temp_text
+    temp_text = temp_text[-1]
+    templ_domain = temp_text.split('Domain: ')
+    # get things within [ ]
+    domain = templ_domain[1].split('[')[1].split(']')[0]
+    # print("Domain: ", domain)
+    # print("Last line: ", temp_text)
+    domain_process_string = "[" + domain + "]"
+    domains = ast.literal_eval(domain_process_string)
+    # print(domains) # domains is a list
+
+    # build instruction
+    # Instruction: Identify slot names and values from the dialogue. The domains are taxi, restaurant. The slots for taxi are leaveAt, destination, departure, arriveBy. The slots for restaurant are people, day, time, food, pricerange, name, area.
+    # if there is only one domain, then the instruction will the "The domain is taxi":
+    if len(domains) == 1:
+        instruction = "Identify slot names and values from the dialogue. The domain is "
+    else:
+        instruction = "Identify slot names and values from the dialogue. The domains are "
+    for i in range(len(domains)):
+        instruction += domains[i]
+        if i != len(domains) - 1:
+            instruction += ", "
+        else:
+            instruction += "."
+    
+    for i in range(len(domains)):
+        instruction += " The slots for "
+        instruction += domains[i] + " are "
+        for j in range(len(dom2slots[domains[i]])):
+            instruction += dom2slots[domains[i]][j]
+            if j != len(dom2slots[domains[i]]) - 1:
+                instruction += ", "
+            else:
+                instruction += "."
+    # print(instruction)
+
+    # breakpoint()
+    # pass
+    instruction = instruction + "\n" 
+    return instruction
+
+def get_details_from_input_file(input_file_name):
+    # input_file_name = 'LLM_input_sentences_UA_PN.txt'
+    if '_UA' in input_file_name:
+        dialog_side = 'UA'
+    elif '_U' in input_file_name:
+        dialog_side = 'U'
+    
+    if '_PN' in input_file_name:
+        punct = 'N'
+    else:
+        punct = 'Y'
+
+    if '_TN' in input_file_name:
+        tag = 'N'
+    else:
+        tag = 'Y'
+
+    return dialog_side, punct, tag
+
+class NewlineStoppingCriteria(StoppingCriteria):
+    def __init__(self, tokenizer, newline_token_id):
+        self.tokenizer = tokenizer
+        self.newline_token_id = newline_token_id
+
+    def __call__(self, input_ids, scores, **kwargs):
+        # Check if the most recent token is a newline
+        return input_ids[0, -1] == self.newline_token_id
+
+def main():
+    args = parse_args()
+
+    # Create output folder
+    output_folder_path = args.output_folder_path
+
+    if args.model_name == 'allenai/OLMo-7B-Instruct':
+        model_name_short = 'O7BI'
+    elif args.model_name == 'mistralai/Mistral-7B-Instruct-v0.3':
+        model_name_short = 'M7BIV03'
+
+    dataset_name = 'mw24'
+    # model_name_short = 'O7BI'
+    # model_name_short = 'M7BIV03'
+    input_data_folder = args.nec_files
+    context_number = args.context_number
+    # dialog_side = args.dialog_side
+    # punct = args.punct
+    # tag = args.tag
+    arg_instruction = args.instruction
+
+    # Get details from the input file
+    dialog_side, punct, tag = get_details_from_input_file(args.input_file)
+    
+    output_folder_name = args.out_folder_prefix + dataset_name + '_' + model_name_short + '_SS_I' + str(arg_instruction) + '_' + str(dialog_side) + '_' + str(context_number) + '_P' + str(punct) + '_T' + str(tag)
+    output_folder_name = os.path.join(output_folder_path, output_folder_name)
+    if not os.path.exists(output_folder_name):
+        os.makedirs(output_folder_name)
+
+    # Model and tokenizer
+    model_name = args.model_name
+    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, torch_dtype=torch.float16, device_map="auto")
+    model = model.cuda()
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    
+    # Get the token ID for newline
+    newline_token_id = tokenizer.encode("\n", add_special_tokens=False)[0]
+
+
+    # Create log file
+    log_folder = os.path.join(output_folder_name, 'log_files')
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+    logg_file = os.path.join(log_folder, str(args.offset_start)+'_log.json')
+    # if log file exists, print the message and exit
+    if os.path.exists(logg_file):
+        print(f"Log file {logg_file} exists. Exiting.")
+        exit()
+
+    # Load input file
+    # input_file = args.input_file
+    input_file = os.path.join(input_data_folder, args.input_file)
+    with open(input_file, 'r') as f:
+        in_file = f.readlines()
+    
+    # Load test file
+    test_tsv_file_path = os.path.join(input_data_folder, 'mw24_DST_test_U_punct_turns.tsv')
+    with open(test_tsv_file_path, 'r') as tst_tsv_file:
+        test_data = tst_tsv_file.readlines()
+
+    # Create config file
+    config_file = os.path.join(output_folder_name, str(args.offset_start)+'_config.json')
+    with open(config_file, 'w') as config_file:
+        config_file.write(json.dumps(vars(args), indent=4))
+        config_file.write('Paramenters extracted from the input file. (Need not consider from the args above, consider from below.)\n')
+        config_file.write(f" dialog side: {dialog_side}, punctuation: {punct}, tag: {tag}")
+        # Add details of the run
+        config_file.write(platform.node())
+        config_file.write('\n')
+        config_file.write(platform.platform())
+        config_file.write('\n')
+        config_file.write(f"Output written at: {output_folder_name}")
+        config_file.write('\n')
+        config_file.write(config_string)
+
+    counter = 0
+    for i, (line, gold_line) in enumerate(zip(in_file, test_data)):
+        if i < args.offset_start:
+            counter += 1
+            continue
+        if i >= args.offset_start + args.offset_add:
+            break
+        counter += 1
+
+        # get the gold line
+        gold_fname, gold_sentence, gold_domain, gold_slots = finding_test_labels(gold_line)
+
+        context = ast.literal_eval(line)[0]
+
+        # get the context number
+        context_len = context.count('\n')
+
+        instruction = ''
+        append_predicted = ''
+        total_input_to_LLM = ''
+        slot_history_dict = ''
+        slot_history_dict_temp = ''
+        slot_history_dict_fin = ''
+        decoded_responses = []
+        # print(gold_domain)
+        for i, dom in enumerate(ast.literal_eval(gold_domain)):
+        # for i, (dom, g_slots) in enumerate(zip(ast.literal_eval(gold_domain), ast.literal_eval(gold_slots))):
+            print(dom)
+            # print(type(dom))
+            if args.instruction == 0:
+                instruction = ''
+            elif args.instruction == 1: 
+                instruction = get_instruction_for_domain(dom)
+            elif args.instruction == 2:
+                instruction = "Identify the slot value.\n"
+            
+            slot_history_dict = slot_history_dict_temp
+            # print(slot_history_dict)
+            # breakpoint()
+            # get the slots for the domain
+            slot_values = dom2slots[dom]
+            if i == 0:
+                slot_history_dict = '\"' + dom + '\": {'
+            else:
+                slot_history_dict += '}, \"' + dom + '\": {'
+
+            for j, slot_key in enumerate(slot_values):
+                # print(f"Slot value for {dom}: is {slot_value}")
+                if j == 0:
+                    slot_history_dict += '\"' + slot_key + '\":'
+                else:
+                    slot_history_dict += ', \"' + slot_key + '\":'
+                
+                total_input_to_LLM = instruction + context + slot_history_dict 
+                # print(total_input_to_LLM)
+                # print("------")
+
+                input_ids = tokenizer.encode(total_input_to_LLM, return_tensors='pt')
+                input_ids = input_ids.cuda()
+                
+                # Define stopping criteria
+                stopping_criteria = StoppingCriteriaList([NewlineStoppingCriteria(tokenizer, newline_token_id)])
+
+                with torch.no_grad():
+                    output = model.generate(
+                        input_ids,
+                        max_new_tokens=12,
+                        return_dict_in_generate=True,
+                        output_scores=False,
+                        stopping_criteria=stopping_criteria
+                    )
+
+                decoded_response = tokenizer.decode(output.sequences[0][input_ids.shape[-1]:], skip_special_tokens=True)
+                decoded_responses.append(decoded_response)
+                # print("------")
+                # print(decoded_response)
+                processed_output = decoded_response.split(',')[0].strip()
+                processed_output = remove_punctuations([processed_output])[0].strip()
+                # print("------")
+                # print(processed_output)
+                # val = "predicted value"
+                val = processed_output
+
+
+
+                # get the slot history
+                slot_history_dict += ' \"' + val + '\"' 
+                # breakpoint()
+
+            # print(slot_history_dict)
+            # slot_history_dict_fin += slot_history_dict
+            slot_history_dict_temp = slot_history_dict 
+            # print(slot_history_dict_temp)
+            # breakpoint()
+        slot_history_dict_final = '{' + slot_history_dict + '}}'
+        print(slot_history_dict_final)
+        print("======")
+        # breakpoint()
+
+
+        log_dictionary = {
+            "line_number": counter,
+            "gold_fname": gold_fname,
+            "gold_domain": gold_domain,
+            "gold_slots": gold_slots,
+            "Instruction": instruction,
+            "Context_length": context_len,
+            "decoded_response": decoded_responses, 
+            "processed_output": slot_history_dict_final,
+            "total_input_to_LLM": total_input_to_LLM
+            }
+
+        with open(logg_file, 'a', encoding='utf-8') as log_file:
+            log_file.write(json.dumps(log_dictionary))
+            log_file.write('\n')
+
+        
+
+if __name__ == '__main__':
+    main()
+
---
+++ scripts/mw21_new/TOD_mw24_5_slot_eval_error_analysis_old.py
@@ -0,0 +1,186 @@
+import numpy as np
+import ast
+import argparse
+import json
+import os
+import TODx as TODx
+import json_repair
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(
+        description='Evaluate open-ended domain-wise slot prediction.'
+    )
+    parser.add_argument(
+        '--eval_folder_path',
+        type=str,
+        default='/mnt/matylda4/hegde/int_ent/TOD_llm/experiments/mw24/O7BI/usr_agnt_ualimtd_TOD_mw24_O7BI_SS_I2_U_10_PY_TY',
+        # default='/mnt/matylda4/hegde/int_ent/TOD_llm/experiments/newer/TOD_mw24_M7BIV03_SS_I2_U_10_PY_TY/',
+        help='Path to the directory containing input JSON files',
+    )
+    args = parser.parse_args()
+    return args
+
+
+def compare_slots(gold_slots, predicted_slots, domain_slots, domain):
+    correct_count = 0
+    total_count = 0
+    total_NA_count = 0
+    correct_NA_count = 0
+    domain_slot_stats = {}
+
+    for slot in domain_slots:
+        domain_slot_key = f"{domain}_{slot}"
+        if domain_slot_key not in domain_slot_stats:
+            domain_slot_stats[domain_slot_key] = {
+                'correct': 0,
+                'total': 0,
+                'NA_correct': 0,
+                'NA_total': 0,
+                'non_NA_correct': 0,
+                'non_NA_total': 0,
+            }
+
+        domain_slot_stats[domain_slot_key]['total'] += 1
+
+        if slot in gold_slots:
+            total_count += 1
+            gold_value = TODx.remove_punctuations([gold_slots[slot]])[0]
+            predicted_value = TODx.remove_punctuations([predicted_slots.get(slot, "").strip()])[0]
+
+            # remove white spaces
+            gold_value = gold_value.replace(" ", "")
+            predicted_value = predicted_value.replace(" ", "")
+
+
+            if gold_value == predicted_value:
+                correct_count += 1
+                domain_slot_stats[domain_slot_key]['correct'] += 1
+
+            if gold_value == "na":
+                total_NA_count += 1
+                domain_slot_stats[domain_slot_key]['NA_total'] += 1
+                if gold_value == predicted_value or predicted_value == "":
+                    correct_NA_count += 1
+                    domain_slot_stats[domain_slot_key]['NA_correct'] += 1
+            else:
+                domain_slot_stats[domain_slot_key]['non_NA_total'] += 1
+                if gold_value == predicted_value:
+                    domain_slot_stats[domain_slot_key]['non_NA_correct'] += 1
+
+    return correct_count, total_count, total_NA_count, correct_NA_count, domain_slot_stats
+
+
+def main():
+    args = arg_parse()
+    eval_file = os.path.join(args.eval_folder_path, 'log_files/')
+    print("Evaluation Started")
+
+    dom2slots = {
+        'taxi': ['leaveAt', 'destination', 'departure', 'arriveBy'],
+        'restaurant': ['people', 'day', 'time', 'food', 'pricerange', 'name', 'area'],
+        'attraction': ['type', 'name', 'area'],
+        'train': ['people', 'leaveAt', 'destination', 'day', 'arriveBy', 'departure'],
+        'hotel': ['stay', 'day', 'people', 'name', 'area', 'parking', 'pricerange', 'stars', 'internet', 'type']
+    }
+
+    total_correct = 0
+    total_slots = 0
+    total_NA_count = 0
+    total_correct_NA_count = 0
+    domain_wise_stats = {
+        domain: {'correct': 0, 'total': 0, 'NA_correct': 0, 'NA_total': 0, 'non_NA_correct': 0, 'non_NA_total': 0}
+        for domain in dom2slots
+    }
+    domain_slot_stats = {}
+
+    for file in os.listdir(eval_file):
+        if file.endswith('.json'):
+            print(f"Processing file: {file}")
+            log_file = os.path.join(eval_file, file)
+
+            with open(log_file, 'r') as f:
+                eval_data = f.readlines()
+
+            for line in eval_data:
+                line_dict = json.loads(line.strip())
+                gold_domain = ast.literal_eval(line_dict['gold_domain'])
+                gold_slots = ast.literal_eval(line_dict['gold_slots'])
+                predicted_output = json_repair.loads(line_dict['processed_output'])
+
+                for g_domain, g_slots in zip(gold_domain, gold_slots):
+                    if g_domain in predicted_output:
+                        predicted_slots = predicted_output[g_domain]
+                        valid_slots = dom2slots.get(g_domain)
+
+                        correct, total, NA_count, correct_NA_count, slot_stats = compare_slots(
+                            g_slots, predicted_slots, valid_slots, g_domain
+                        )
+
+                        total_correct += correct
+                        total_slots += total
+                        total_NA_count += NA_count
+                        total_correct_NA_count += correct_NA_count
+
+                        domain_wise_stats[g_domain]['correct'] += correct
+                        domain_wise_stats[g_domain]['total'] += total
+                        domain_wise_stats[g_domain]['NA_correct'] += correct_NA_count
+                        domain_wise_stats[g_domain]['NA_total'] += NA_count
+                        domain_wise_stats[g_domain]['non_NA_correct'] += correct - correct_NA_count
+                        domain_wise_stats[g_domain]['non_NA_total'] += total - NA_count
+
+                        for key, stats in slot_stats.items():
+                            if key not in domain_slot_stats:
+                                domain_slot_stats[key] = {
+                                    'correct': 0,
+                                    'total': 0,
+                                    'NA_correct': 0,
+                                    'NA_total': 0,
+                                    'non_NA_correct': 0,
+                                    'non_NA_total': 0,
+                                }
+                            domain_slot_stats[key]['correct'] += stats['correct']
+                            domain_slot_stats[key]['total'] += stats['total']
+                            domain_slot_stats[key]['NA_correct'] += stats['NA_correct']
+                            domain_slot_stats[key]['NA_total'] += stats['NA_total']
+                            domain_slot_stats[key]['non_NA_correct'] += stats['non_NA_correct']
+                            domain_slot_stats[key]['non_NA_total'] += stats['non_NA_total']
+
+    # Final Evaluation Summary
+    print("===============")
+    print("Evaluation Summary")
+    print(f"Total Correct Slots: {total_correct}")
+    print(f"Total Slots: {total_slots}")
+    accuracy = (total_correct / total_slots) * 100 if total_slots > 0 else 0
+    print(f"Slot Prediction Accuracy: {accuracy:.2f}%")
+    print(f"Total NA Slots: {total_NA_count}")
+    print(f"Correct NA Slots: {total_correct_NA_count}")
+    NA_accuracy = (total_correct_NA_count / total_NA_count) * 100 if total_NA_count > 0 else 0
+    print(f"NA Accuracy: {NA_accuracy:.2f}%")
+    Total_non_NA_slots = total_slots - total_NA_count
+    Total_correct_non_NA_slots = total_correct - total_correct_NA_count
+    Non_na_accuracy = (Total_correct_non_NA_slots / Total_non_NA_slots) * 100 if Total_non_NA_slots > 0 else 0
+    print(f"Non NA Accuracy: {Non_na_accuracy:.2f}%")
+    print("===============")
+
+    # Save results
+    folder_name = args.eval_folder_path.split('/')[-2]
+    out_file = os.path.join(args.eval_folder_path, folder_name + '_results.json')
+    with open(out_file, 'w') as f:
+        json.dump({
+            'total_correct': total_correct,
+            'total_slots': total_slots,
+            'accuracy': accuracy,
+            'total_NA_count': total_NA_count,
+            'total_correct_NA_count': total_correct_NA_count,
+            'NA_accuracy': NA_accuracy,
+            'Total_non_NA_slots': Total_non_NA_slots,
+            'Total_correct_non_NA_slots': Total_correct_non_NA_slots,
+            'Non_na_accuracy': Non_na_accuracy,
+            'domain_wise_stats': domain_wise_stats,
+            'domain_slot_stats': domain_slot_stats
+        }, f, indent=4)
+
+
+if __name__ == '__main__':
+    main()
